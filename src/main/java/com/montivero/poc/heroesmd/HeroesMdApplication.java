4d6c6caf67716981ed76c5f7ca5174adea9ea101
Daniel.Montivero <montivero.daniel.e@gmail.com>
2022-02-28 11:22:57 -0300

    Add initial boilerplate

---
+++ src/main/java/com/montivero/poc/heroesmd/HeroesMdApplication.java
@@ -0,0 +1,16 @@
+package com.montivero.poc.heroesmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class HeroesMdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesMdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montivero/poc/heroesmd/HeroesMdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montivero.poc.heroesmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesMdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
